dc515f07bd6bed4b540397419c8b2d7181e4888f
sukhachev <sukhachev@i-teco.ru>
2019-11-20 17:01:12 +0500

    add network example

---
+++ src/main/java/network/interact/client/Client.java
@@ -0,0 +1,33 @@
+package network.interact.client;
+
+import network.interact.Listener;
+import network.interact.server.Server;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+    public static Integer CLIENT_PORT = 4998;
+
+    public static void main(String[] args) {
+        Listener listenerThread = new Listener(CLIENT_PORT);
+        listenerThread.start();
+        try (Socket socket = new Socket("127.0.0.1", Server.SERVER_PORT);
+             BufferedWriter bufferedWriter = new BufferedWriter(
+                     new OutputStreamWriter(socket.getOutputStream()))) {
+            Scanner scanner = new Scanner(System.in);
+            String message;
+            while (!(message = scanner.nextLine()).isEmpty()) {
+                bufferedWriter.write(message);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
